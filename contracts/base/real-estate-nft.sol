2ce39bb6782cdd81b381c8af75d5ac3ef50e53e4
davidkayce <david@korapay.com>
2021-08-09 00:17:15 +0200

    feat: update smart contract

--- contracts/base/real-estate-nft.sol
+++ contracts/base/real-estate-nft.sol
@@ -16,14 +16,13 @@ contract RealEstateNFT is ERC721 {
      * @notice We need to know all the stakeholders in the system.
      */
     address public issuer; // originator of the asset (normally the company)
-    address[] internal stakeholders; // all the people who have a stake in the asset
 
     // Transaction properties
     address public txFeeToken;
     uint256 public txFeeAmount;
+    uint256 public tokenValue;
     uint256 internal accumulated; // total amount of undistributed revenue
-    mapping(address => bool) public excludedList;
-    mapping(address => uint256) internal revenues; // The accumulated revenue for each stakeholder.
+    mapping(address => bool) public excludedList; // list of addresses that are excluded from paying fees
 
     /**
      * @notice The constructor for the Real Estate NFT. This contract represents 
@@ -35,14 +34,85 @@ contract RealEstateNFT is ERC721 {
      */
     constructor(
         address _issuer,
-        uint256 _supply,
-        address _txFeeToken,
-        uint256 _txFeeAmount
-    ) ERC721("My NFT", "ABC") {
-      issuer = _issuer;
-      txFeeToken = _txFeeToken;
-      txFeeAmount = _txFeeAmount;
-      excludedList[_artist] = true;
-      _mint(_owner, _supply);
+        uint256 _value,
+        uint256 _segmentNumber,
+        uint256 _segmentSize,
+        address _txFeeToken
+    ) ERC721("ProperTee", "PRPT") {
+        issuer = _issuer;
+        txFeeToken = _txFeeToken;
+        tokenValue = _value;
+        txFeeAmount = 0.1 * 10**18;
+        excludedList[_issuer] = true;
     }
+
+    function createHouseAsset(uint256 seed, string memory tokenURI)
+        returns (bytes32)
+    {
+        // TODO: Handle hashing seed with 
+        _safeMint(_issuer, requestId); // Replace with itemID
+    }
+
+    // Transfers and implementing transfer fee
+    function setExcluded(address excluded, bool status) external {
+        require(msg.sender == artist, "artist only");
+        excludedList[excluded] = status;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override {
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: transfer caller is not owner nor approved"
+        );
+        if (excludedList[from] == false) {
+            _payTxFee(from);
+        }
+        _transfer(from, to, tokenId);
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override {
+        if (excludedList[from] == false) {
+            _payTxFee(from);
+        }
+        safeTransferFrom(from, to, tokenId, "");
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) public override {
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: transfer caller is not owner nor approved"
+        );
+        if (excludedList[from] == false) {
+            _payTxFee(from);
+        }
+        _safeTransfer(from, to, tokenId, _data);
+    }
+
+    function _payTxFee(address from) internal {
+        IERC20 token = IERC20(txFeeToken);
+        token.safeTransferFrom(from, artist, txFeeAmount);
+    }
+
+    // Marketplace
+    function payout(
+        uint256 tokenId,
+        uint256 amount,
+        address issuer,
+        address receipient
+    ) public {}
+
+    function updateValue(uint256 tokenId, uint256 interest) public {}
 }
