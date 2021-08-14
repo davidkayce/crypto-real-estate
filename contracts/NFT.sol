c14e6c45423be4b64a03c2fee14fd1d5f0bd40aa
Divine Olokor <divee789@gmail.com>
2021-08-14 19:34:49 +0100

    update: suggested updates

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -11,8 +11,8 @@ contract RealEstateNFT is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    address manager;
-    int listingId;
+    address public manager;
+    int256 public listingId;
 
     constructor(
         bytes32 memory tokenName,
@@ -20,11 +20,22 @@ contract RealEstateNFT is ERC721 {
         address _manager,
         address _owner,
         bytes32 memory _tokenURI,
-        uint _listingId
+        uint256 _listingId
     ) public ERC721(tokenName, symbol) {
         manager = _manager;
         listingId = _listingId;
         mintAsset(_owner, _tokenURI);
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI)
+        internal
+        virtual
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI set of nonexistent token"
+        );
+        _tokenURIs[tokenId] = _tokenURI;
     }
 
     function mintAsset(address owner, bytes32 memory tokenURI)
@@ -32,12 +43,12 @@ contract RealEstateNFT is ERC721 {
         returns (uint256)
     {
         _tokenIds.increment();
-        int assetId = _tokenIds.current();
+        int256 assetId = _tokenIds.current();
 
         _safeMint(owner, assetId);
         _setTokenURI(assetId, tokenURI);
         setApprovalForAll(manager, true);
-        
+
         return assetId;
     }
 }
